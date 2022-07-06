1d90bf3d6737a9c605e7793c7a98bdb105ff96f3
Gin <tecpoirot@foxmail.com>
2022-07-06 13:17:10 +0800

    chore: 修补小问题
    1、完善 Readme
    2、构建脚本调整输出文件名称
    3、增加测试代码

--- test/app_test.go
+++ test/app_test.go
@@ -1,31 +1,55 @@
 package test_test
 
 import (
-	cert "check_cert"
+	cert "check_cert/pkg"
 	"io/ioutil"
+	"log"
 	"testing"
 )
 
-func TestCertChecker1(t *testing.T) {
-	pemStr, _ := ioutil.ReadFile("/Volumes/Data/MyCode/Learning/Golang/CheckCertSignatureFrom/cert_file/1.crt")
-	derData, _ := ioutil.ReadFile("/Volumes/Data/MyCode/Learning/Golang/CheckCertSignatureFrom/cert_file/2.der")
-	cert1, _ := cert.NewCertificateWrapperFromPEMString(string(pemStr))
-	cert2, _ := cert.NewCertificateWrapperFromDERData(derData)
-	if err := cert1.CheckSignatureFrom(cert2); err == nil {
-		t.Log("OK")
-	} else {
-		t.Log(err.Error())
-	}
+//func TestCertChecker1(t *testing.T) {
+//	pemStr, _ := ioutil.ReadFile("/Volumes/Data/MyCode/Learning/Golang/CheckCertSignatureFrom/cert_file/1.crt")
+//	derData, _ := ioutil.ReadFile("/Volumes/Data/MyCode/Learning/Golang/CheckCertSignatureFrom/cert_file/2.der")
+//	cert1, _ := cert.NewCertificateWrapperFromPEMString(string(pemStr))
+//	cert2, _ := cert.NewCertificateWrapperFromDERData(derData)
+//	if err := cert1.CheckSignatureFrom(cert2); err == nil {
+//		t.Log("OK")
+//	} else {
+//		t.Log(err.Error())
+//	}
+//}
+//
+//func TestCertChecker2(t *testing.T) {
+//	derData, _ := ioutil.ReadFile("/Volumes/Data/MyCode/Learning/Golang/CheckCertSignatureFrom/cert_file/1.der")
+//	pemStr, _ := ioutil.ReadFile("/Volumes/Data/MyCode/Learning/Golang/CheckCertSignatureFrom/cert_file/2.crt")
+//	cert2, _ := cert.NewCertificateWrapperFromPEMString(string(pemStr))
+//	cert1, _ := cert.NewCertificateWrapperFromDERData(derData)
+//	if err := cert1.CheckSignatureFrom(cert2); err == nil {
+//		t.Log("OK")
+//	} else {
+//		t.Log(err.Error())
+//	}
+//}
+
+const CERT_PATH = "/Volumes/Data/MyCode/Learning/Golang/gomobile-demo/test/cert_file/"
+const CRT1 = CERT_PATH + "1.crt"
+const CRT2 = CERT_PATH + "2.crt"
+
+func loadPEMCertStrFromPEMCertFile(pemFileName string) string {
+	pemData, _ := ioutil.ReadFile(pemFileName)
+	return string(pemData)
 }
 
-func TestCertChecker2(t *testing.T) {
-	derData, _ := ioutil.ReadFile("/Volumes/Data/MyCode/Learning/Golang/CheckCertSignatureFrom/cert_file/1.der")
-	pemStr, _ := ioutil.ReadFile("/Volumes/Data/MyCode/Learning/Golang/CheckCertSignatureFrom/cert_file/2.crt")
-	cert2, _ := cert.NewCertificateWrapperFromPEMString(string(pemStr))
-	cert1, _ := cert.NewCertificateWrapperFromDERData(derData)
-	if err := cert1.CheckSignatureFrom(cert2); err == nil {
-		t.Log("OK")
-	} else {
-		t.Log(err.Error())
-	}
+func loadPEMPubKeyStrFromPEMCertStr(pemCertFile string) string {
+	pemCertStr := loadPEMCertStrFromPEMCertFile(pemCertFile)
+	certWrapper, _ := cert.NewCertificateWrapperFromPEMString(pemCertStr)
+	pubKeyPEMStr, _ := certWrapper.PublicKeyPEMString()
+	return pubKeyPEMStr
+}
+
+func TestPublicKey(t *testing.T) {
+	pbk1Str := loadPEMPubKeyStrFromPEMCertStr(CRT1)
+	pbk2Str := loadPEMPubKeyStrFromPEMCertStr(CRT2)
+	log.Println(pbk1Str)
+	log.Println(pbk2Str)
 }
